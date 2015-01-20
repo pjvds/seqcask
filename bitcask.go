5065093398d20bf26bb145db29bf0eaad0fd621e
Pieter Joost van de Sande <pj@born2code.net>
2015-01-20 21:27:13 +0100

    adds Keydir

--- bitcask.go
+++ bitcask.go
@@ -7,6 +7,7 @@ import (
 	"io/ioutil"
 	"os"
 	"path"
+	"sync"
 
 	"github.com/OneOfOne/xxhash"
 )
@@ -18,6 +19,64 @@ var (
 type Bitcask struct {
 	buffer     *bytes.Buffer
 	activeFile *os.File
+
+	keydir *Keydir
+}
+
+type Item struct {
+	Fid    uint16
+	Vsz    uint16
+	Vpos   uint32
+	Tstamp uint64
+}
+
+type Keydir struct {
+	shards     []map[string]Item
+	shardLocks []sync.RWMutex
+}
+
+func NewKeydir() *Keydir {
+	shards := make([]map[string]Item, 256)
+	for i := 0; i < len(shards); i++ {
+		shards[i] = make(map[string]Item, 1024)
+	}
+
+	return &Keydir{
+		shards:     shards,
+		shardLocks: make([]sync.RWMutex, 256, 256),
+	}
+}
+
+func (this *Keydir) Add(key []byte, fid, vsz uint16, vpos uint32, tstamp uint64) {
+	fb := key[0]
+	shard := this.shards[fb]
+	lock := this.shardLocks[fb]
+
+	lock.Lock()
+	shard[string(key)] = Item{fid, vsz, vpos, tstamp}
+	lock.Unlock()
+}
+
+func (this *Keydir) Get(key []byte) (*Item, bool) {
+	fb := key[0]
+	shard := this.shards[fb]
+	lock := this.shardLocks[fb]
+
+	lock.RLock()
+	item, ok := shard[string(key)]
+	lock.RUnlock()
+
+	return &item, ok
+}
+
+func (this *Keydir) Remove(key string) {
+	fb := key[0]
+	shard := this.shards[fb]
+	lock := this.shardLocks[fb]
+
+	lock.Lock()
+	delete(shard, string(key))
+	lock.Unlock()
 }
 
 type header struct {
@@ -44,23 +103,28 @@ func Open(directory string) (*Bitcask, error) {
 	return &Bitcask{
 		activeFile: file,
 		buffer:     new(bytes.Buffer),
+		keydir:     NewKeydir(),
 	}, nil
 }
 
 func (this *Bitcask) Put(key []byte, value []byte) (err error) {
 	ksz := uint16(len(key))
 	vsz := uint16(len(value))
+	//tstamp := time.Now().UnixNano()
 
 	binary.Write(this.buffer, binary.LittleEndian, ksz) // 2 bytes
 	binary.Write(this.buffer, binary.LittleEndian, vsz) // 2 bytes
+	//binary.Write(this.buffer, binary.LittleEndian, tstamp)
 	this.buffer.Write(key)
 	this.buffer.Write(value)
 
-	length := 2 + 2 + len(key) + len(value)
+	length := 4 + len(key) + len(value)
 	bufferSlice := this.buffer.Bytes()
 	dataToCrc := bufferSlice[len(bufferSlice)-length:]
 	checksum := xxhash.Checksum64(dataToCrc)
 	binary.Write(this.buffer, binary.LittleEndian, checksum)
+
+	this.keydir.Add(key, ksz, vsz, 0, 0)
 	return
 }
 
