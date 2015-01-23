0b4d91958b1405f9a1c7568a580e19f8d98d216a
Pieter Joost van de Sande <pj@born2code.net>
2015-01-23 20:49:47 +0100

    adds tests for WriteBatch

--- batch_test.go
+++ batch_test.go
@@ -1,11 +1,28 @@
 package seqcask_test
 
 import (
-    "testing"
-    "github.com/stretchr/testify"
+	"bytes"
+	"testing"
+
+	"github.com/pjvds/seqcask"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestNewWriteBatch(t *testing.T) {
-    batch := seqcask.NewWriteBatch()
-    assert.NotNil(t, batch)
+	batch := seqcask.NewWriteBatch()
+	assert.NotNil(t, batch)
+}
+
+func TestWrite(t *testing.T) {
+	batch := seqcask.NewWriteBatch()
+
+	value := []byte("foobar")
+	batch.Put(value)
+
+	buffer := new(bytes.Buffer)
+	batch.Write(0, buffer)
+
+	// length should be:
+	// offset (uint64) + value size (uint32) + value + checksum (uint64)
+	assert.Equal(t, 8+2+len(value)+8, buffer.Len())
 }
