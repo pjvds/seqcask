6429a147904512f4f7187fec7eb9771314cc9daa
Pieter Joost van de Sande <pj@born2code.net>
2015-01-25 13:27:44 +0100

    fix the tests

--- seqdir_test.go
+++ seqdir_test.go
@@ -11,7 +11,7 @@ func BenchmarkSeqDirAdds(b *testing.B) {
 
 	b.StartTimer()
 	for iteration := 0; iteration < b.N; iteration++ {
-		seqdir.Add(uint64(iteration), 0, 0, 0)
+		seqdir.Add(uint64(iteration), 0, 0)
 	}
 	b.StopTimer()
 }
@@ -19,16 +19,14 @@ func BenchmarkSeqDirAdds(b *testing.B) {
 func TestAddGet(t *testing.T) {
 	seqdir := NewSeqDir()
 	sequence := uint64(0)
-	fileId := uint16(6)
 	valueSize := uint32(12)
 	position := int64(88)
 
-	seqdir.Add(sequence, fileId, valueSize, position)
+	seqdir.Add(sequence, valueSize, position)
 
 	item, ok := seqdir.Get(sequence)
 
 	assert.True(t, ok)
-	assert.Equal(t, item.FileId, fileId)
 	assert.Equal(t, valueSize, item.ValueSize)
 	assert.Equal(t, position, item.Position)
 }
@@ -37,12 +35,10 @@ func TestAddAllGetAll(t *testing.T) {
 	seqdir := NewSeqDir()
 
 	sequence := uint64(0)
-	fileId := uint16(1)
 	valueSize := uint32(12)
 
 	items := make([]Item, 255, 255)
 	for index, item := range items {
-		item.FileId = fileId
 		item.ValueSize = valueSize
 		item.Position = int64(index) * int64(valueSize)
 	}
