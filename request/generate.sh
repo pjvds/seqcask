4de4d78e5a11d8cdfeb9f8e57e27430062777851
Pieter Joost van de Sande <pj@born2code.net>
2015-02-11 22:15:05 +0100

    adds script to generate types from proto definitions

---
+++ request/generate.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+# Generates new go files based on the proto files
+
+# install required tools
+go get -u github.com/golang/protobuf/{proto,protoc-gen-go}
+
+# check if go bin is in path
+if [[ $PATH != *"$GOPATH/bin"* ]]
+then
+    echo "\$GOPATH/bin not in PATH!"
+    exit 1
+fi
+
+protoc --go_out=. *.proto
