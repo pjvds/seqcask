aa7601e42f66b6ea6bac4163817d461e760302cd
Pieter Joost van de Sande <pj@born2code.net>
2015-02-12 21:03:33 +0100

    remove proto definitions and types

--- request/types.go
+++ request/types.go
@@ -3,3 +3,11 @@ package request
 const (
 	T_Append = byte(iota)
 )
+
+func NewAppendBody(topic string, partition uint32, bodies [][]byte) AppendBody {
+	return AppendBody{
+		Topic:     &topic,
+		Partition: &partition,
+		Messages:  bodies,
+	}
+}
