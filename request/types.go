a5a16ae85f248a6e7ae9d42e07ba4a32190ae1c9
Pieter Joost van de Sande <pj@born2code.net>
2015-02-12 21:55:24 +0100

    fix request package

--- request/types.go
+++ request/types.go
@@ -3,11 +3,3 @@ package request
 const (
 	T_Append = byte(iota)
 )
-
-func NewAppendBody(topic string, partition uint32, bodies [][]byte) AppendBody {
-	return AppendBody{
-		Topic:     &topic,
-		Partition: &partition,
-		Messages:  bodies,
-	}
-}
